31844c645a6e0be062db0d1f210846ea977a05dc
Shubhro Ghosh <shubhrogh765@gmail.com>
2026-01-08 17:22:08 +0530

    server download disabled on production

--- express-backend/src/utils/serverDownloadWithProgress.js
+++ express-backend/src/utils/serverDownloadWithProgress.js
@@ -4,8 +4,28 @@ import { progressMap } from "./progressStore.js";
 import { db, fileDownloads } from "../db/index.js";
 import { eq } from "drizzle-orm";
 
+const isServerDownloadEnabled = process.env.SERVER_DOWNLOAD_ENABLED ?? false
+
 
 export async function serverDownloadWithProgress(id, url, options = { fileName: null }) {
+
+    if(isServerDownloadEnabled !== "true"){
+         await db.update(fileDownloads).set({
+            status: "failed",
+            errorMessage: "server download not available",
+            updatedAt: new Date(),
+        }).where(eq(fileDownloads.id, id))
+
+        progressMap.set(id, {
+            "downloadedBytes":null,
+            "totalBytes":null,
+            "percentFixed2": null,
+            "percent": null,
+            "done": true
+        })
+
+        return null
+    }
 
     const response = await fetch(url);
     if (!response.ok) {
